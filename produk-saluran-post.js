7c252c32dd564230078b4ba1ab4cbb563c1f6c04
aliyul <waliprogrammer@gmail.com>
2025-03-30 04:49:46 +0700

    Update and rename produk-konstruksi-post.js to produk-saluran-post.js

---
+++ produk-saluran-post.js
@@ -0,0 +1,262 @@
+
+ // Cek URL saat ini dan sesuaikan dengan kondisi yang diinginkan
+const urlMappingBuis = {
+"https://www.betonjayareadymix.com/2021/03/harga-buis-beton-paling-murah.html": "Harga Buis Beton Paling Murah",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-precon.html": "Buis Beton Precon",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-proyek.html": "Buis Beton Proyek",
+    "https://www.betonjayareadymix.com/2023/02/buis-beton-tanpa-lubang.html": "Buis Beton Tanpa Lubang",
+    "https://www.betonjayareadymix.com/2023/02/buis-beton-pori-lubang.html": "Buis Beton Pori Lubang",
+    "https://www.betonjayareadymix.com/2023/02/buis-beton-persegi.html": "Buis Beton Persegi",
+    "https://www.betonjayareadymix.com/2023/02/buis-beton-u.html": "Buis Beton Bentuk U",
+    "https://www.betonjayareadymix.com/2023/02/buis-beton-kolam.html": "Buis Beton Kolam",
+    "https://www.betonjayareadymix.com/2023/02/buis-beton-pondasi-cyclop.html": "Buis Beton Pondasi Cyclop",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-resapan.html": "Buis Beton Resapan",
+  "https://www.betonjayareadymix.com/2021/03/buis-beton-rcp.html": "Buis Beton RCP",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-riol.html": "Buis Beton Riol",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-sumur-resapan.html": "Buis Beton Sumur Resapan",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-saluran.html": "Buis Beton Saluran",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-80-x-50.html": "Buis Beton 80 x 50",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-100-x-100.html": "Buis Beton 100 x 100",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-tangerang.html": "Buis Beton Tangerang",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-serang.html": "Buis Beton Serang",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-cisangkan.html": "Buis Beton Cisangkan",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-murah.html": "Buis Beton Murah",
+  "https://www.betonjayareadymix.com/2021/03/buis-beton-purwakarta.html": "Buis Beton Purwakarta",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-septic-tank.html": "Buis Beton Septic Tank",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-diameter-20-cm.html": "Buis Beton Diameter 20 cm",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-jakarta.html": "Buis Beton Jakarta",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-karawang.html": "Buis Beton Karawang",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-diameter-150-cm.html": "Buis Beton Diameter 150 cm",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-diameter-80-cm.html": "Buis Beton Diameter 80 cm",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-gorong-gorong.html": "Buis Beton Gorong Gorong",
+  "https://www.betonjayareadymix.com/2021/03/buis-beton-diameter-50-cm.html": "Buis Beton Diameter 50 CM",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-diameter-30-cm.html": "Buis Beton Diameter 30 CM",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-diameter-60-cm.html": "Buis Beton Diameter 60 CM",
+    "https://www.betonjayareadymix.com/2021/03/buis-beton-setengah-lingkaran.html": "Buis Beton Setengah Lingkaran",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-scg.html": "Buis Beton SCG",
+    "https://www.betonjayareadymix.com/2022/01/pipa-scg.html": "Pipa SCG",
+    "https://www.betonjayareadymix.com/2022/01/pipa-beton-scg.html": "Pipa Beton SCG",
+    "https://www.betonjayareadymix.com/2022/01/manhole-scg.html": "Manhole SCG",
+    "https://www.betonjayareadymix.com/2022/01/sumur-resapan-buis-beton-scg.html": "Sumur Resapan Buis Beton SCG",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-kotak.html": "Buis Beton Kotak",
+  "https://www.betonjayareadymix.com/2022/01/buis-beton-u.html": "Buis Beton U",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-bulat.html": "Buis Beton Bulat",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-diameter-100-cm.html": "Buis Beton Diameter 100 CM",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-terdekat.html": "Buis Beton Terdekat",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-asiacon.html": "Buis Beton Asiacom",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-saluran-air.html": "Buis Beton Saluran Air",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-bandung.html": "Buis Beton Bandung",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-belah.html": "Buis Beton Belah",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-bertulang.html": "Buis Beton Bertulang",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-bogor.html": "Buis Beton Bogor",
+  "https://www.betonjayareadymix.com/2022/01/buis-beton-bekasi.html": "Buis Beton Bekasi",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-biopori.html": "Buis Beton Biopori",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-cirebon.html": "Buis Beton Cirebon",
+    "https://www.betonjayareadymix.com/2022/01/buis-beton-depok.html": "Buis Beton Depok",
+
+  "https://www.betonjayareadymix.com/2021/03/harga-buis-beton.html": "Harga Buis Beton",
+    "https://www.betonjayareadymix.com/2021/08/harga-buis-beton-tanpa-tulangan.html": "Harga Buis Beton Tanpa Tulangan",
+    "https://www.betonjayareadymix.com/2021/08/harga-buis-beton-bulat.html": "Harga Buis Beton Bulat",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-asiacon.html": "Harga Buis Beton Asiacom",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-dusaspun.html": "Harga Buis Beton Dusaspun",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-saluran-air.html": "Harga Buis Beton Saluran Air",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-bertulang.html": "Harga Buis Beton Bertulang",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-belah.html": "Harga Buis Beton Belah",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-buat-sumur.html": "Harga Buis Beton Buat Sumur",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-bandung.html": "Harga Buis Beton Bandung",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-bekasi.html": "Harga Buis Beton Bekasi",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-bogor.html": "Harga Buis Beton Bogor",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-diameter-100-cm.html": "Harga Buis Beton Diameter 100 CM",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-diameter-50-cm.html": "Harga Buis Beton Diameter 50 CM",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-diameter-30-cm.html": "Harga Buis Beton Diameter 30 CM",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-diameter-150-cm.html": "Harga Buis Beton Diameter 150 CM",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-diameter-80-cm.html": "Harga Buis Beton Diameter 80 CM",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-diameter-40-cm.html": "Harga Buis Beton Diameter 40 CM",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-karawang.html": "Harga Buis Beton Karawang",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-cirebon.html": "Harga Buis Beton Cirebon",
+  "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-untuk-saluran-air.html": "Harga Buis Beton untuk Saluran Air",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-bertulang-diameter-100.html": "Harga Buis Beton Bertulang Diameter 100 cm",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-80-cm.html": "Harga Buis Beton 80 cm",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-kecil.html": "Harga Buis Beton Kecil",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-sepiteng.html": "Harga Buis Beton Septic Tank",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-selokan.html": "Harga Buis Beton Selokan",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-termurah.html": "Harga Buis Beton Termurah",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-wc.html": "Harga Buis Beton WC",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-jakarta.html": "Harga Buis Beton Jakarta",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-tangerang.html": "Harga Buis Beton Tangerang",
+  "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-12-diameter-20-cm.html": "Harga Buis Beton 12 Diameter 20 CM",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-12-dia-30-cm.html": "Harga Buis Beton 1/2 Dia 30 CM",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-setengah-lingkaran.html": "Harga Buis Beton Setengah Lingkaran",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-rcp.html": "Harga Buis Beton RCP",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-semarang.html": "Harga Buis Beton Semarang",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-12-lingkaran.html": "Harga Buis Beton 1/2 Lingkaran",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-lingkaran.html": "Harga Buis Beton Lingkaran",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-gorong-gorong.html": "Harga Buis Beton Gorong Gorong",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-gresik.html": "Harga Buis Beton Gresik",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-80-cm-jogja.html": "Harga Buis Beton 80 CM Jogja",
+  "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-surabaya.html": "Harga Buis Beton Surabaya",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-kotak.html": "Harga Buis Beton Kotak",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-sumur-resapan.html": "Harga Buis Beton Sumur Resapan",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-100-x-100.html": "Harga Buis Beton 100 x 100",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-diameter-90.html": "Harga Buis Beton Diameter 90",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-mojokerto.html": "Harga Buis Beton Mojokerto",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-tasikmalaya.html": "Harga Buis Beton Tasikmalaya",
+    "https://www.betonjayareadymix.com/2021/03/harga-buis-beton-80-x-100.html": "Harga Buis Beton 80 x 100",
+
+  
+    "https://www.betonjayareadymix.com/2021/03/ukuran-buis-beton-saluran-air.html": "Ukuran Buis Beton Saluran Air",
+    "https://www.betonjayareadymix.com/2021/03/ukuran-buis-beton-belah.html": "Ukuran Buis Beton Belah",
+    "https://www.betonjayareadymix.com/2021/03/ukuran-buis-beton-untuk-sumur.html": "Ukuran Buis Beton untuk Sumur",
+    "https://www.betonjayareadymix.com/2021/03/ukuran-buis-beton-untuk-septic-tank.html": "Ukuran Buis Beton untuk Septic Tank",
+    "https://www.betonjayareadymix.com/2021/03/ukuran-buis-beton-u.html": "Ukuran Buis Beton U",
+    "https://www.betonjayareadymix.com/2021/03/ukuran-buis-beton-diameter-80-cm.html": "Ukuran Buis Beton Diameter 80 CM",
+    "https://www.betonjayareadymix.com/2021/03/ukuran-buis-beton-diameter-100-cm.html": "Ukuran Buis Beton Diameter 100 CM",
+
+  "https://www.betonjayareadymix.com/2021/03/jual-buis-beton-bertulang.html": "Jual Buis Beton Bertulang",
+    "https://www.betonjayareadymix.com/2021/03/jual-buis-beton-dusaspun.html": "Jual Buis Beton Dusaspun",
+    "https://www.betonjayareadymix.com/2021/03/jual-buis-beton-murah.html": "Jual Buis Beton Murah",
+    "https://www.betonjayareadymix.com/2021/03/jual-buis-beton-terdekat.html": "Jual Buis Beton Terdekat",
+    "https://www.betonjayareadymix.com/2021/03/jual-buis-beton-bekasi.html": "Jual Buis Beton Bekasi",
+  "https://www.betonjayareadymix.com/2021/03/jual-buis-beton-bandung.html": "Jual Buis Beton Bandung",
+    "https://www.betonjayareadymix.com/2021/03/jual-buis-beton-di-bogor.html": "Jual Buis Beton di Bogor",
+    "https://www.betonjayareadymix.com/2021/03/jual-buis-beton-depok.html": "Jual Buis Beton Depok",
+    "https://www.betonjayareadymix.com/2021/03/jual-buis-beton-bali.html": "Jual Buis Beton Bali"
+};
+
+};
+const urlMappingBoxCulvert = {
+    "https://www.betonjayareadymix.com/2021/04/arti-box-culvert.html": "Arti Box Culvert",
+  "https://www.betonjayareadymix.com/2021/04/kegunaan-box-culvert.html": "Kegunaan Box Culvert",
+  "https://www.betonjayareadymix.com/2021/04/kekuatan-box-culvert.html": "Kekuatan Box Culvert",
+  "https://www.betonjayareadymix.com/2021/04/kekurangan-box-culvert.html": "Kekurangan Box Culvert",
+  "https://www.betonjayareadymix.com/2021/04/kelebihan-box-culvert.html": "Kelebihan Box Culvert",
+  "https://www.betonjayareadymix.com/2021/04/box-culvert-bandung.html": "Box Culvert Bandung",
+  "https://www.betonjayareadymix.com/2021/04/box-culvert-jalan-tol.html": "Box Culvert Jalan Tol",
+  "https://www.betonjayareadymix.com/2021/04/box-culvert-jogja.html": "Box Culvert Jogja",
+  "https://www.betonjayareadymix.com/2021/04/box-culvert-jakarta.html": "Box Culvert Jakarta",
+  "https://www.betonjayareadymix.com/2021/04/box-culvert-jawatimur.html": "Box Culvert Jawa Timur"
+
+
+};
+const urlMappingBobokBeton = {
+    "https://www.betonjayareadymix.com/2019/06/harga-bongkar-beton.html": "Harga Bongkar Beton",
+    "https://www.betonjayareadymix.com/2019/06/harga-bobok-beton-per-m2.html":  "Harga Bobok Beton Per M2"
+
+
+};
+const urlMappingBongkarKeramik = {
+    "https://www.betonjayareadymix.com/2019/06/biaya-bongkar-lantai-keramik.html": "Biaya Bongkar Lantai Keramik",
+    "https://www.betonjayareadymix.com/2019/06/harga-bongkar-lantai-keramik.html": "Harga Bongkar Lantai Keramik"
+
+};
+
+// Menyimpan elemen yang dihapus dalam variabel
+let removedElementsJasaPerbaikanKons = {};
+// Fungsi untuk menghapus elemen berdasarkan ID
+function removeCondition(conditionId) {
+    const conditionElement = document.getElementById(conditionId);
+
+    if (conditionElement) {
+        // Menyimpan elemen yang dihapus dalam objek untuk bisa dikembalikan
+        removedElementsJasaPerbaikanKons[conditionId] = conditionElement;
+        conditionElement.remove(); // Menghapus elemen tersebut
+    }
+}
+
+// Fungsi untuk mengembalikan elemen yang telah dihapus
+function restoreCondition(conditionId) {
+    const breadcrumb = document.querySelector('.breadcrumb');
+    const elementToRestore = removedElementsJasaPerbaikanKons[conditionId]; // Mendapatkan elemen yang disimpan
+
+    if (elementToRestore) {
+        breadcrumb.appendChild(elementToRestore); // Menambahkan elemen kembali ke dalam breadcrumb
+        delete removedElementsJasaPerbaikanKons[conditionId]; // Menghapus elemen dari objek setelah dikembalikan
+    } else {
+        console.log(`Elemen dengan ID ${conditionId} tidak ditemukan di removedElements.`);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // var currentUrl = window.location.href;
+     //const cleanUrl = currentUrl.split('?')[0]; // Menghapus parameter seperti ?m=1
+    const cleanUrlJasaPerbaikanKons = window.location.href.split(/[?#]/)[0]; // Menghilangkan parameter seperti ?m=1
+
+     // Menemukan elemen menggunakan Id
+    // var materialKons = document.getElementById("materialKons");
+    // var JasaKons = document.getElementById("JasaKons");
+      var JasaKonstruksiPerbaikanLink = document.getElementById("JasaKonstruksiPerbaikan");
+     var JasaRenovasiPerbaikanLink = document.getElementById("JasaRenovasiPerbaikan");
+     var JasaChippingBetonLink = document.getElementById("JasaChippingBeton");
+     var JasaBobokTembokLink = document.getElementById("JasaBobokTembok");
+     var JasaBobokBetonLink = document.getElementById("JasaBobokBeton");
+     var JasaBongkarKeramikLink = document.getElementById("JasaBongkarKeramik");
+     var pageNameKonstruksiPerbaikan = document.getElementById("pageNameKonstruksiPerbaikan");
+    
+
+     // Default untuk menyembunyikan elemen
+     // materialKons.style.visibility = 'hidden';
+      //JasaKons.style.visibility = 'hidden';
+ 
+     JasaKonstruksiPerbaikanLink.style.visibility = 'hidden';
+     JasaRenovasiPerbaikanLink.style.visibility = 'hidden';
+     JasaChippingBetonLink.style.visibility = 'hidden';
+     JasaBobokTembokLink.style.visibility = 'hidden';
+     JasaBobokBetonLink.style.visibility = 'hidden';
+     JasaBongkarKeramikLink.style.visibility = 'hidden';
+     pageNameKonstruksiPerbaikan.textContent = "";
+
+    if (urlMappingChippingBeton[cleanUrlJasaPerbaikanKons]) {
+        restoreCondition('JasaKonsPerbaikan');
+        restoreCondition('JasaChippingBeton');
+     
+        removeCondition('materialKonsReadymix');
+        removeCondition('JasaBobokTembok');
+        removeCondition('JasaBongkarKeramik');
+        removeCondition('JasaBobokBeton');
+       
+        JasaKonstruksiPerbaikanLink.style.visibility = 'visible';
+        JasaRenovasiPerbaikanLink.style.visibility = 'visible';
+        JasaChippingBetonLink.style.visibility = 'visible';
+        pageNameKonstruksi.textContent = urlMappingChippingBeton[cleanUrlJasaPerbaikanKons];
+    }
+    if (urlMappingBobokTembok[cleanUrlJasaPerbaikanKons]) {
+        removeCondition('materialKonsReadymix');
+        restoreCondition('JasaKonsPerbaikan');
+     
+        removeCondition('JasaChippingBeton');
+        removeCondition('JasaBongkarKeramik');
+       removeCondition('JasaBobokBeton');
+        restoreCondition('JasaBobokTembok');
+        JasaKonstruksiPerbaikanLink.style.visibility = 'visible';
+        JasaRenovasiPerbaikanLink.style.visibility = 'visible';
+        JasaBobokTembokLink.style.visibility = 'visible';
+        pageNameKonstruksi.textContent = urlMappingBobokTembok[cleanUrlJasaPerbaikanKons];
+    }
+   if (urlMappingBobokBeton[cleanUrlJasaPerbaikanKons]) {
+       removeCondition('materialKonsReadymix');
+       restoreCondition('JasaKonsPerbaikan');
+    
+        removeCondition('JasaChippingBeton');
+        removeCondition('JasaBongkarKeramik');
+       removeCondition('JasaBobokTembok');
+        restoreCondition('JasaBobokBeton');
+        JasaKonstruksiPerbaikanLink.style.visibility = 'visible';
+        JasaRenovasiPerbaikanLink.style.visibility = 'visible';
+        JasaBobokBetonLink.style.visibility = 'visible';
+        pageNameKonstruksi.textContent = urlMappingBobokBeton[cleanUrlJasaPerbaikanKons];
+    }
+   if (urlMappingBongkarKeramik[cleanUrlJasaPerbaikanKons]) {
+        removeCondition('materialKonsReadymix');
+        restoreCondition('JasaKonsPerbaikan');
+    
+        removeCondition('JasaChippingBeton');
+        removeCondition('JasaBobokTembok');
+         removeCondition('JasaBobokBeton');
+        restoreCondition('JasaBongkarKeramik');
+        JasaKonstruksiPerbaikanLink.style.visibility = 'visible';
+        JasaRenovasiPerbaikanLink.style.visibility = 'visible';
+        JasaBongkarKeramikLink.style.visibility = 'visible';
+        pageNameKonstruksi.textContent = urlMappingBongkarKeramik[cleanUrlJasaPerbaikanKons];
+    }
+
+   });
