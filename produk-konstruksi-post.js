7edcb8a7c269fbaa2bb92982cc9415c338e2851b
aliyul <waliprogrammer@gmail.com>
2025-04-28 16:19:14 +0700

    Update produk-konstruksi-post.js

--- produk-konstruksi-post.js
+++ produk-konstruksi-post.js
@@ -4,117 +4,27 @@
   "https://www.betonjayareadymix.com/p/alat-konstruksi.html": "Alat Konstruksi",
 */
  // Cek URL saat ini dan sesuaikan dengan kondisi yang diinginkan
-const urlMappingProdukPagarPanelBeton = {
-  "https://www.betonjayareadymix.com/2018/09/harga-pagar-panel-beton.html": "Harga Pagar Panel Beton",
-  "https://www.betonjayareadymix.com/2018/09/pagar-panel-beton-murah.html": "Pagar Panel Beton Murah",
-  "https://www.betonjayareadymix.com/2018/09/harga-pagar-panel-beton-murah.html": "Harga Pagar Panel Beton Murah",
-  "https://www.betonjayareadymix.com/2018/09/harga-pagar-panel-beton-pracetak.html": "Harga Pagar Panel Beton Pracetak",
-  "https://www.betonjayareadymix.com/2018/09/harga-pagar-panel-beton-per-meter.html": "Harga Pagar Panel Beton Per Meter",
-  "https://www.betonjayareadymix.com/2018/09/pagar-panel-beton-precast.html": "Pagar Panel Beton Precast",
-  "https://www.betonjayareadymix.com/2018/09/ukuran-pagar-panel-beton.html": "Ukuran Pagar Panel Beton",
-  "https://www.betonjayareadymix.com/2018/09/pagar-panel-beton-motif.html": "Pagar Panel Beton Motif",
-  "https://www.betonjayareadymix.com/2018/09/jual-pagar-panel-beton-precast.html": "Jual Pagar Panel Beton Precast",
-  "https://www.betonjayareadymix.com/2018/09/harga-tiang-pagar-panel-beton.html": "Harga Tiang Pagar Panel Beton",
-
- "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-jakarta.html": "Pagar Panel Beton Jakarta",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-bogor.html": "Pagar Panel Beton Bogor",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-depok.html": "Pagar Panel Beton Depok",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-tangerang.html": "Pagar Panel Beton Tangerang",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-bekasi.html": "Pagar Panel Beton Bekasi",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-bandung.html": "Pagar Panel Beton Bandung",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-cianjur.html": "Pagar Panel Beton Cianjur",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-purwakarta.html": "Pagar Panel Beton Purwakarta",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-tasikmalaya.html": "Pagar Panel Beton Tasikmalaya",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-cirebon.html": "Pagar Panel Beton Cirebon",
- "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-indramayu.html": "Pagar Panel Beton Indramayu",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-sukabumi.html": "Pagar Panel Beton Sukabumi",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-subang.html": "Pagar Panel Beton Subang",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-karawang.html": "Pagar Panel Beton Karawang",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-majalengka.html": "Pagar Panel Beton Majalengka",
-  "https://www.betonjayareadymix.com/2018/11/harga-pagar-panel-beton-jakarta.html": "Harga Pagar Panel Beton Jakarta",
-  "https://www.betonjayareadymix.com/2018/11/harga-pagar-panel-beton-bogor.html": "Harga Pagar Panel Beton Bogor",
-  "https://www.betonjayareadymix.com/2018/11/harga-pagar-panel-beton-karawang.html": "Harga Pagar Panel Beton Karawang",
-  "https://www.betonjayareadymix.com/2018/11/harga-pagar-panel-beton-depok.html": "Harga Pagar Panel Beton Depok",
-  "https://www.betonjayareadymix.com/2018/11/harga-pagar-panel-beton-bekasi.html": "Harga Pagar Panel Beton Bekasi",
- "https://www.betonjayareadymix.com/2018/11/harga-pagar-panel-beton-tangerang.html": "Harga Pagar Panel Beton Tangerang",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-garut.html": "Pagar Panel Beton Garut",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-ciamis.html": "Pagar Panel Beton Ciamis",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-kuningan.html": "Pagar Panel Beton Kuningan",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-semarang.html": "Pagar Panel Beton Semarang",
-  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-polos.html": "Pagar Panel Beton Polos",
-  "https://www.betonjayareadymix.com/2018/11/harga-pagar-panel-beton-motif.html": "Harga Pagar Panel Beton Motif",
-  "https://www.betonjayareadymix.com/2018/11/harga-pagar-panel-beton-polos.html": "Harga Pagar Panel Beton Polos",
-  "https://www.betonjayareadymix.com/2018/11/harga-pagar-panel-beton-per-lembar.html": "Harga Pagar Panel Beton Per Lembar",
-  "https://www.betonjayareadymix.com/2018/11/biaya-pagar-panel-beton-per-meter.html": "Biaya Pagar Panel Beton Per Meter"
-
+const urlMappingProdukBetonPrecast = {
 
 };
-const urlMappingProdukPagarBeton = {
-"https://www.betonjayareadymix.com/2018/09/harga-pagar-beton.html": "Harga Pagar Beton",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-murah.html": "Pagar Beton Murah",
-  "https://www.betonjayareadymix.com/2018/09/jenis-pagar-beton.html": "Jenis Pagar Beton",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-motif.html": "Pagar Beton Motif",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-pracetak.html": "Pagar Beton Pracetak",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-per-meter.html": "Pagar Beton Per Meter",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-pembatas-tanah.html": "Pagar Beton Pembatas Tanah",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-keliling.html": "Pagar Beton Keliling",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-kawat-duri.html": "Pagar Beton Kawat Duri",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-jalan-tol.html": "Pagar Beton Jalan Tol",
-   "https://www.betonjayareadymix.com/2018/09/pagarbeton-jadi.html": "Pagar Beton Jadi",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-eser.html": "Pagar Beton Eser",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-gazebo.html": "Pagar Beton Gazebo",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-batu-alam.html": "Pagar Beton Batu Alam",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-precast.html": "Pagar Beton Precast",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-modern.html": "Pagar Beton Modern",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-balkon.html": "Pagar Beton Balkon",
-  "https://www.betonjayareadymix.com/2018/09/tiang-pagar-beton.html": "Tiang Pagar Beton",
-  "https://www.betonjayareadymix.com/2018/09/harga-pagar-beton-per-meter.html": "Harga Pagar Beton Per Meter",
-  "https://www.betonjayareadymix.com/2018/09/pagar-rumah-beton.html": "Pagar Rumah Beton",
- "https://www.betonjayareadymix.com/2018/09/pagar-beton-rumah-minimalis.html": "Pagar Beton Rumah Minimalis",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-minimalis.html": "Pagar Beton Minimalis",
-  "https://www.betonjayareadymix.com/2018/09/jual-pagar-beton-precast.html": "Jual Pagar Beton Precast",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-lantai-2.html": "Pagar Beton Lantai 2",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-depan-rumah.html": "Pagar Beton Depan Rumah",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-cantik.html": "Pagar Beton Cantik",
-  "https://www.betonjayareadymix.com/2018/09/pagar-beton-terbaik.html": "Pagar Beton Terbaik",
-  "https://www.betonjayareadymix.com/2018/11/pagar-beton-bogor.html": "Pagar Beton Bogor",
-  "https://www.betonjayareadymix.com/2018/11/pagar-beton-depok.html": "Pagar Beton Depok",
-  "https://www.betonjayareadymix.com/2018/11/pagar-beton-bekasi.html": "Pagar Beton Bekasi",
-  "https://www.betonjayareadymix.com/2018/11/pagar-beton-tangerang.html": "Pagar Beton Tangerang",
- "https://www.betonjayareadymix.com/2018/11/pagar-beton-bandung.html": "Pagar Beton Bandung",
-  "https://www.betonjayareadymix.com/2018/11/pagar-beton-karawang.html": "Pagar Beton Karawang",
-  "https://www.betonjayareadymix.com/2018/11/pagar-beton-cianjur.html": "Pagar Beton Cianjur",
-  "https://www.betonjayareadymix.com/2018/11/pagar-beton-cirebon.html": "Pagar Beton Cirebon",
-  "https://www.betonjayareadymix.com/2018/11/pagar-beton-indramayu.html": "Pagar Beton Indramayu",
-  "https://www.betonjayareadymix.com/2018/11/pagar-beton-purwakarta.html": "Pagar Beton Purwakarta",
-  "https://www.betonjayareadymix.com/2018/11/pagar-beton-sukabumi.html": "Pagar Beton Sukabumi",
-  "https://www.betonjayareadymix.com/2018/11/pagar-beton-tasikmalaya.html": "Pagar Beton Tasikmalaya",
-  "https://www.betonjayareadymix.com/2018/11/pagar-beton-subang.html": "Pagar Beton Subang",
-  "https://www.betonjayareadymix.com/2018/11/harga-pagar-beton-jakarta.html": "Harga Pagar Beton Jakarta",
- "https://www.betonjayareadymix.com/2018/11/harga-pagar-beton-bogor.html": "Harga Pagar Beton Bogor",
-    "https://www.betonjayareadymix.com/2018/11/harga-pagar-beton-depok.html": "Harga Pagar Beton Depok",
-    "https://www.betonjayareadymix.com/2018/11/ukuran-pagar-beton.html": "Ukuran Pagar Beton",
-    "https://www.betonjayareadymix.com/2018/11/harga-pagar-beton-bekasi.html": "Harga Pagar Beton Bekasi",
-    "https://www.betonjayareadymix.com/2018/11/harga-pagar-beton-tangerang.html": "Harga Pagar Beton Tangerang",
-    "https://www.betonjayareadymix.com/2018/11/harga-pagar-beton-karawang.html": "Harga Pagar Beton Karawang",
-    "https://www.betonjayareadymix.com/2018/11/harga-pagar-beton-motif.html": "Harga Pagar Beton Motif"
- 
+const urlMappingProdukPanelBetonPrecast = {
+};
+const urlMappingProdukBetonRinganPrecast = {
 
 };
-const urlMappingProdukPagarRumah = {
- "https://www.betonjayareadymix.com/2018/11/model-pagar-rumah-minimalis-modern.html": "Model Pagar Rumah Minimalis Modern"
-};
+const urlMappingProdukAlatKonstruksi = {
 
+};
 
 // Menyimpan elemen yang dihapus dalam variabel
-let removedElementsProdukPembatasKons = {};
+let removedElementsProdukKonsPost = {};
 // Fungsi untuk menghapus elemen berdasarkan ID
 function removeCondition(conditionId) {
     const conditionElement = document.getElementById(conditionId);
 
     if (conditionElement) {
         // Menyimpan elemen yang dihapus dalam objek untuk bisa dikembalikan
-        removedElementsProdukPembatasKons[conditionId] = conditionElement;
+        removedElementsProdukKonsPost[conditionId] = conditionElement;
         conditionElement.remove(); // Menghapus elemen tersebut
     }
 }
@@ -122,20 +32,20 @@ function removeCondition(conditionId) {
 // Fungsi untuk mengembalikan elemen yang telah dihapus
 function restoreCondition(conditionId) {
     const breadcrumb = document.querySelector('.breadcrumb');
-    const elementToRestore = removedElementsProdukPembatasKons[conditionId]; // Mendapatkan elemen yang disimpan
+    const elementToRestore = removedElementsProdukKonsPost[conditionId]; // Mendapatkan elemen yang disimpan
 
     if (elementToRestore) {
         breadcrumb.appendChild(elementToRestore); // Menambahkan elemen kembali ke dalam breadcrumb
-        delete removedElementsProdukPembatasKons[conditionId]; // Menghapus elemen dari objek setelah dikembalikan
+        delete removedElementsProdukKonsPost[conditionId]; // Menghapus elemen dari objek setelah dikembalikan
     } else {
-        console.log(`Elemen dengan ID ${conditionId} tidak ditemukan di removedElementsProdukPembatasKons.`);
+        console.log(`Elemen dengan ID ${conditionId} tidak ditemukan di removedElementsProdukKonsPost.`);
     }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     // var currentUrl = window.location.href;
      //const cleanUrl = currentUrl.split('?')[0]; // Menghapus parameter seperti ?m=1
-    const cleanUrlProdukPembatasKons = window.location.href.split(/[?#]/)[0]; // Menghilangkan parameter seperti ?m=1
+    const cleanUrlProdukKonsPost = window.location.href.split(/[?#]/)[0]; // Menghilangkan parameter seperti ?m=1
 
      // Menemukan elemen menggunakan Id
     var ProdukKonsPembatasPost = document.getElementById("ProdukKonsPembatasPost");
@@ -161,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function() {
      //JasaBongkarKeramikLink.style.visibility = 'hidden';
      pageNameProdukPembatas.textContent = "";
 
-    if (urlMappingProdukPagarPanelBeton[cleanUrlProdukPembatasKons]) {
+    if (urlMappingProdukBetonPrecast[cleanUrlProdukKonsPost]) {
         restoreCondition('ProdukKonsPembatasPost');
         restoreCondition('ProdukPagarPanelBeton');
      
@@ -186,61 +96,7 @@ document.addEventListener("DOMContentLoaded", function() {
         ProdukKonstruksiPembatasLink.style.visibility = 'visible';
         ProdukPembatasLink.style.visibility = 'visible';
         ProdukPagarPanelBetonLink.style.visibility = 'visible';
-        pageNameProdukPembatas.textContent = urlMappingProdukPagarPanelBeton[cleanUrlProdukPembatasKons];
+        pageNameProdukPembatas.textContent = urlMappingProdukBetonPrecast[cleanUrlProdukKonsPost];
     }
-    if (urlMappingProdukPagarBeton[cleanUrlProdukPembatasKons]) {
-        restoreCondition('ProdukKonsPembatasPost');
-        restoreCondition('ProdukPagarBeton');
-
-       //hapus elemen div id lain
-        removeCondition('MaterialKonsStukturPost');
-        removeCondition('MaterialKonsFasadPelapisEksteriorPost');
-        removeCondition('MaterialKonsAtapPenutupPost');
-        removeCondition('ProdukKonsSaluranPost');
-        removeCondition('JasaKonsPerbaikanPost');
-        removeCondition('JasaKonsPembatasPost');
-        removeCondition('JasaKonsPondasiTanahPost');
-        removeCondition('JasaKonsPerkuatanTanahLongsorPost');
-	removeCondition('JasaKonsBongkarBangunanPost');
-        removeCondition('JasaKonsPengeboranPost'); 
-	removeCondition('JasaKonsFinishingPost');
-        removeCondition('JasaKonsStrukturPost');
-     
-        //hapus elemen jasa pembatas lain
-        removeCondition('ProdukPagarPanelBeton');
-        removeCondition('ProdukPagarRumah');
-        
-        ProdukKonstruksiPembatasLink.style.visibility = 'visible';
-        ProdukPembatasLink.style.visibility = 'visible';
-        ProdukPagarBetonLink.style.visibility = 'visible';
-        pageNameProdukPembatas.textContent = urlMappingProdukPagarBeton[cleanUrlProdukPembatasKons];
-    }
-   if (urlMappingProdukPagarRumah[cleanUrlProdukPembatasKons]) {
-        restoreCondition('ProdukKonsPembatasPost');
-        restoreCondition('ProdukPagarRumah');
-
-       //hapus elemen div id lain
-        removeCondition('MaterialKonsStukturPost');
-        removeCondition('MaterialKonsFasadPelapisEksteriorPost');
-        removeCondition('MaterialKonsAtapPenutupPost');
-        removeCondition('ProdukKonsSaluranPost');
-        removeCondition('JasaKonsPerbaikanPost');
-        removeCondition('JasaKonsPembatasPost');
-        removeCondition('JasaKonsPondasiTanahPost');
-        removeCondition('JasaKonsPerkuatanTanahLongsorPost');
-	removeCondition('JasaKonsBongkarBangunanPost');
-        removeCondition('JasaKonsPengeboranPost'); 
-	removeCondition('JasaKonsFinishingPost');
-        removeCondition('JasaKonsStrukturPost');
-     
-        //hapus elemen jasa pembatas lain
-        removeCondition('ProdukPagarPanelBeton');
-        removeCondition('ProdukPagarBeton');
     
-        ProdukKonstruksiPembatasLink.style.visibility = 'visible';
-        ProdukPembatasLink.style.visibility = 'visible';
-        ProdukPagarRumahLink.style.visibility = 'visible';
-        pageNameProdukPembatas.textContent = urlMappingProdukPagarRumah[cleanUrlProdukPembatasKons];
-    }
- 
    });
