fb47fd500695d9819b0f24909bb6e547187f602d
aliyul <waliprogrammer@gmail.com>
2025-04-28 16:48:37 +0700

    Update produk-konstruksi-post.js

--- produk-konstruksi-post.js
+++ produk-konstruksi-post.js
@@ -5,6 +5,22 @@
 */
  // Cek URL saat ini dan sesuaikan dengan kondisi yang diinginkan
 const urlMappingProdukBetonPrecast = {
+"https://www.betonjayareadymix.com/2022/01/scg-pipe-and-precast.html": "SCG Pipe And Precast",
+  "https://www.betonjayareadymix.com/2021/04/merak-jaya-beton-precast.html": "Merak Jaya Beton Precast",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-indonesia.html": "Beton Precast Indonesia",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-untuk-jalan.html": "Beton Precast Untuk Jalan",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-jogja.html": "Beton Precast Jogja",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-depok.html": "Beton Precast Depok",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-bogor.html": "Beton Precast Bogor",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-bekasi.html": "Beton Precast Bekasi",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-subang.html": "Beton Precast Subang",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-bandung.html": "Beton Precast Bandung",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-ruspin.html": "Beton Precast Ruspin",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-untuk-saluran-irigasi.html": "Beton Precast Untuk Saluran Irigasi",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-untuk-lantai.html": "Beton Precast Untuk Lantai",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-untuk-rumah.html": "Beton Precast Untuk Rumah",
+  "https://www.betonjayareadymix.com/2021/04/beton-precast-hcs.html": "Beton Precast HCS",
+  "https://www.betonjayareadymix.com/2021/04/wika-beton-precast.html": "Wika Beton Precast"
 
 };
 const urlMappingProdukPanelBetonPrecast = {
@@ -101,5 +117,91 @@ document.addEventListener("DOMContentLoaded", function() {
         ProdukBetonPrecastLink.style.visibility = 'visible';
         pageNameProdukKonsPost.textContent = urlMappingProdukBetonPrecast[cleanUrlProdukKonsPost];
     }
-    
+    if (urlMappingProdukPanelBetonPrecast[cleanUrlProdukKonsPost]) {
+        restoreCondition('ProdukKonsPost');
+        restoreCondition('ProdukPanelBetonPrecast');
+     
+        //hapus elemen div id lain
+        removeCondition('MaterialKonsStukturPost');
+        removeCondition('MaterialKonsFasadPelapisEksteriorPost');
+        removeCondition('MaterialKonsAtapPenutupPost');
+        removeCondition('ProdukKonsSaluranPost');
+        removeCondition('ProdukKonsPembatasPost');
+        removeCondition('JasaKonsPerbaikanPost');
+        removeCondition('JasaKonsPembatasPost');
+        removeCondition('JasaKonsPondasiTanahPost');
+        removeCondition('JasaKonsPerkuatanTanahLongsorPost');
+	removeCondition('JasaKonsBongkarBangunanPost');
+        removeCondition('JasaKonsPengeboranPost'); 
+	removeCondition('JasaKonsFinishingPost');
+        removeCondition('JasaKonsStrukturPost');
+
+        //hapus elemen ProdukKonstruksiPost lain SELAIN ProdukBetonPrecast
+       // removeCondition('');
+        removeCondition('ProdukBetonPrecast');
+        removeCondition('ProdukBetonRinganPrecast');
+        removeCondition('ProdukAlatKonstruksi');
+       
+        ProdukKonstruksiPostLink.style.visibility = 'visible';
+        ProdukPanelBetonPrecastLink.style.visibility = 'visible';
+        pageNameProdukKonsPost.textContent = urlMappingProdukPanelBetonPrecast[cleanUrlProdukKonsPost];
+    }
+   if (urlMappingProdukBetonRinganPrecast[cleanUrlProdukKonsPost]) {
+        restoreCondition('ProdukKonsPost');
+        restoreCondition('ProdukBetonRinganPrecast');
+     
+        //hapus elemen div id lain
+        removeCondition('MaterialKonsStukturPost');
+        removeCondition('MaterialKonsFasadPelapisEksteriorPost');
+        removeCondition('MaterialKonsAtapPenutupPost');
+        removeCondition('ProdukKonsSaluranPost');
+        removeCondition('ProdukKonsPembatasPost');
+        removeCondition('JasaKonsPerbaikanPost');
+        removeCondition('JasaKonsPembatasPost');
+        removeCondition('JasaKonsPondasiTanahPost');
+        removeCondition('JasaKonsPerkuatanTanahLongsorPost');
+	removeCondition('JasaKonsBongkarBangunanPost');
+        removeCondition('JasaKonsPengeboranPost'); 
+	removeCondition('JasaKonsFinishingPost');
+        removeCondition('JasaKonsStrukturPost');
+
+        //hapus elemen ProdukKonstruksiPost lain SELAIN ProdukBetonPrecast
+       // removeCondition('');
+        removeCondition('ProdukPanelBetonPrecast');
+        removeCondition('ProdukBetonPrecast');
+        removeCondition('ProdukAlatKonstruksi');
+       
+        ProdukKonstruksiPostLink.style.visibility = 'visible';
+        ProdukBetonRinganPrecastLink.style.visibility = 'visible';
+        pageNameProdukKonsPost.textContent = urlMappingProdukBetonRinganPrecast[cleanUrlProdukKonsPost];
+    }
+   if (urlMappingProdukAlatKonstruksi[cleanUrlProdukKonsPost]) {
+        restoreCondition('ProdukKonsPost');
+        restoreCondition('ProdukAlatKonstruksi');
+     
+        //hapus elemen div id lain
+        removeCondition('MaterialKonsStukturPost');
+        removeCondition('MaterialKonsFasadPelapisEksteriorPost');
+        removeCondition('MaterialKonsAtapPenutupPost');
+        removeCondition('ProdukKonsSaluranPost');
+        removeCondition('ProdukKonsPembatasPost');
+        removeCondition('JasaKonsPerbaikanPost');
+        removeCondition('JasaKonsPembatasPost');
+        removeCondition('JasaKonsPondasiTanahPost');
+        removeCondition('JasaKonsPerkuatanTanahLongsorPost');
+	removeCondition('JasaKonsBongkarBangunanPost');
+        removeCondition('JasaKonsPengeboranPost'); 
+	removeCondition('JasaKonsFinishingPost');
+        removeCondition('JasaKonsStrukturPost');
+
+        //hapus elemen ProdukKonstruksiPost lain SELAIN ProdukBetonPrecast
+       // removeCondition('');
+        removeCondition('ProdukPanelBetonPrecast');
+        removeCondition('ProdukBetonRinganPrecast');
+        removeCondition('ProdukBetonPrecast');
+       
+        ProdukKonstruksiPostLink.style.visibility = 'visible';
+        ProdukAlatKonstruksiLink.style.visibility = 'visible';
+        pageNameProdukKonsPost.textContent = urlMappingProdukAlatKonstruksi[cleanUrlProdukKonsPost];
+    }
    });
