b32949808f2175fef6e6381b25c707dd1e857697
aliyul <waliprogrammer@gmail.com>
2025-06-18 23:52:03 +0700

    Update produk-konstruksi-post.js

--- produk-konstruksi-post.js
+++ produk-konstruksi-post.js
@@ -66,6 +66,8 @@ const urlMappingProdukBetonRinganPrecast = {
 const urlMappingProdukAlatKonstruksi = {
 "https://www.betonjayareadymix.com/2019/03/harga-mesin-bor-tambang.html": "Harga Mesin Bor Tambang",
 "https://www.betonjayareadymix.com/2019/03/jual-mesin-bor-tambang.html": "Jual Mesin Bor Tambang",
+"https://www.betonjayareadymix.com/2019/03/harga-alat-bor-tambang.html": "Harga Alat Bor Tambang",
+"https://www.betonjayareadymix.com/2019/03/jual-alat-bor-tambang.html": "Jual Alat Bor Tambang",
 	
 "https://www.betonjayareadymix.com/2019/02/harga-alat-tiang-pancang.html": "Harga Alat Tiang Pancang",
   "https://www.betonjayareadymix.com/2019/02/harga-alat-berat-terbaru.html": "Harga Alat Berat Terbaru",
